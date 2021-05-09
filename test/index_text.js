9e67553227ed635a7d16f7ae218898ab7bec04dd
mikejguo290 <mikejguo290@gmail.com>
2021-05-09 21:38:41 +0100

    2. write a test to check if Calculate.factorial(5)===120;.

--- test/index_text.js
+++ test/index_text.js
@@ -3,6 +3,13 @@ var Calculate =  require('../index.js')
 
 describe('Calculate', () => {
   describe('.factorial', () => {
-
+    it('outputs 120 when given 5', ()=>{
+      //Setup
+      const expectedResult=120;
+      //Exercise
+      const result=Calculate.factorial(5);
+      //Verify
+      assert.strictEqual(result, expectedResult)
+    })
   });
 });
