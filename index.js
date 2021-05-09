832802c8d32e3f59ac1ecfebd2a1c6c09c4f7897
mikejguo290 <mikejguo290@gmail.com>
2021-05-09 22:26:03 +0100

    9. Added to implementation code to make edge case test pass.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const Calculate = {
   factorial(inputNumber){
+    if (inputNumber===0){
+      return 1;
+    }
+    
     let numberArray=Array.from(Array(inputNumber).keys()) // create an array of length inputNumber from the keys of the iterable Array(inputNumber)'s index
     numberArray.shift() // removes the first element, which is 0
     numberArray.push(inputNumber) // adds inputNumber to the end as it used to end and inputNumber-1
